7f7ef41aa23bd08f334767837c72b95a2ac4d110
Ian Boucher <i.a.boucher@gmail.com>
2016-07-20 19:50:38 +0100

    Complete cp22 assignment

--- scripts/landing.js
+++ scripts/landing.js
@@ -5,35 +5,16 @@ var animatePoints = function() {
     /* This script transitions the styles from those given in the .css 
     file to those specified here. */
 
-    var revealFirstPoint = function() 
+    function revealPoint(i) 
     {
-        points[0].style.opacity         = 1;
-        points[0].style.transform       = "scaleX(1) translateY(0)";
-        points[0].style.msTransform     = "scaleX(1) translateY(0)";
-        points[0].style.WebkitTransform = "scaleX(1) translateY(0)";
+        points[i].style.opacity         = 1;
+        points[i].style.transform       = "scaleX(1) translateY(0)";
+        points[i].style.msTransform     = "scaleX(1) translateY(0)";
+        points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
     };
-
-    var revealSecondPoint = function() 
+    
+    for (var i = 0; i < points.length; i++)
     {
-        points[1].style.opacity         = 1;
-        points[1].style.transform       = "scaleX(1) translateY(0)";
-        points[1].style.msTransform     = "scaleX(1) translateY(0)";
-        points[1].style.WebkitTransform = "scaleX(1) translateY(0)";
+        revealPoint(i);
     };
-
-    var revealThirdPoint = function() 
-    {
-        points[2].style.opacity         = 1;
-        points[2].style.transform       = "scaleX(1) translateY(0)";
-        points[2].style.msTransform     = "scaleX(1) translateY(0)";
-        points[2].style.WebkitTransform = "scaleX(1) translateY(0)";
-    };
-
-    revealFirstPoint();
-    revealSecondPoint();
-    revealThirdPoint();
-    console.log("I can't see any animation")
-
 };
-
-animatePoints();
